392c1376e4a10fdc0ff11801cb5f93d938d35c8e
Sarah Guthrie <sarah.guthrie16@stjohns.edu>
2020-03-26 12:51:40 -0400

    Encouragment message flashes for 3 second/adding explanation to the json file

--- Milestone One/functions.js
+++ Milestone One/functions.js
@@ -2,12 +2,13 @@
 document.addEventListener('DOMContentLoaded',
 function(){
   document.getElementById("quiz_select_submit").onclick = chose;
-  document.getElementById("quiz_one_questions").onclick = score;
+  document.getElementById("quiz_one_questions").onclick = timed_encouragment;
   document.getElementById("quiz_two_questions").onclick = encouragment;
 })
 let counter = 0;
 function chose(){
   document.getElementById("login").style.display = 'none';
+  document.getElementById("encouragment_grid").style.display = 'none';
   if (document.getElementById("quizes").selectedIndex == 0) {
     document.getElementById("quiz1").style.display = 'block';
   }
@@ -19,13 +20,31 @@ function chose(){
 function score(){
     var input = document.getElementById("#firstname").value
     alert(input);
-
+}
+function timed_encouragment(){
+  setTimeout(encouragment, 0000)
+  setTimeout(chose, 3000)
+  return false;
 }
 
 function encouragment(){
-  var encouragment = ["Goodjob", "Welldone", "Keep It Going"]
+  var encouragment = ["Goodjob", "Well Done", "Keep It Going"]
   var randomEncourgment = encouragment[Math.floor(Math.random()*encouragment.length)];
-  alert(randomEncourgment);
-
-  // document.getElementById("encouragment").style.display = 'block';
+  document.getElementById("login").style.display = 'none';
+  document.getElementById("quiz1").style.display = 'none';
+  document.getElementById("quiz2").style.display = 'none';
+  document.getElementById("encouragment_grid").style.display = 'block';
+  document.getElementById("encouragment").innerHTML = randomEncourgment;
 }
+
+// const fetch = require("node-fetch");
+// async function fetch_users() {
+//   try{
+//     const response = await fetch('https://my-json-server.typicode.com/sguthrie16/CUS1172_Project_4/db')
+//     const result = await response.json()
+//     console.log(result)
+//   }catch(err){
+//     console.error(err);
+//   }
+// }
+// fetch_users()
