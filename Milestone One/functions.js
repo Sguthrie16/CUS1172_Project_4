707160a6c4f9d8a966dd94383f51bb247b97b130
Sarah Guthrie <sarah.guthrie16@stjohns.edu>
2020-03-27 19:02:35 -0400

    Generate questions for each Quiz

--- Milestone One/functions.js
+++ Milestone One/functions.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded',
 function(){
   document.getElementById("quiz_select_submit").onclick =()=>{
-    get_api_data('https://my-json-server.typicode.com/sguthrie16/CUS1172_Project_4/db')
+    get_api_data('https://my-json-server.typicode.com/sguthrie16/CUS1172_Project_4/db'),
     generate_quiz_one()
     };
   document.getElementById("quiz_one_questions").onsubmit =timed_encouragment;
@@ -10,8 +10,7 @@ function(){
   // document.getElementById("quiz_two_questions").onclick = encouragment;
 })
 count = 0;
-counter =0;
-data="";
+
 // Async await - Getting data from quiz
   let get_api_data = async (api_endpoint) =>{
     results = await fetch(api_endpoint);
@@ -19,108 +18,51 @@ data="";
       document.getElementById("login").style.display = 'none'
       document.getElementById("encouragment_grid").style.display = 'none'
       document.getElementById("quiz1").style.display = 'block'
-
-        current_question = data.Questions_Quiz1[count];
-        number_of_questions = Object.keys(data).length;
-        generate_quiz_one(data,current_question,number_of_questions);
-
-      else if (document.getElementById("quiz_select_submit").value == "Quiz2"){
-        current_question = data.Questions_Quiz2[count];
-        number_of_questions = Object.keys(data).length;
-        generate_quiz_two(data,current_question,number_of_questions);
-      }
-
+      current_question = data.Questions_Quiz1[count];
+      number_of_questions = Object.keys(data.Questions_Quiz1).length;
+      question_type = current_question.question_type;
+      generate_quiz_one(data,current_question,number_of_questions,question_type);
 }
-function generate_quiz_one(data,current_question,number_of_questions){
-  for (var i = 0; i < number_of_questions; i++) {
+function generate_quiz_one(data,current_question,number_of_questions,question_type){
 
-    if(current_question.question_type == "Multiple choice"){
+  for (var i = 0; i < number_of_questions; i++) {
+    if(question_type == "Multiple choice"){
       document.getElementById("quiz_one_questions").innerHTML = `
-        <form>
-        <h2>${current_question.question}</h2>
-          <input type="radio" value = ${current_question.options.optiona} name = ${current_question.options.optiona}>
-          <label for=${current_question.options.optiona}> ${current_question.options.optiona}</label><br>
-          <input type="radio" value = ${current_question.options.optionb} name = ${current_question.options.optionb}>
-          <label for=${current_question.options.optionb}>${current_question.options.optionb}</label><br>
-          <input type="radio" value = ${current_question.options.optionc} name = ${current_question.options.optionc}>
-          <label for=${current_question.options.optionc}> ${current_question.options.optionc}</label><br>
-          <input type="radio" value = ${current_question.options.optiond} name=${current_question.options.optiond}>
-          <label>${current_question.options.optiond}</label><br>
-          <input type ="submit" value = "Submit" id="submit">
-        </form>`
-    }
-    else if (current_question.question_type == "TF") {
-      document.getElementById("quiz_one_questions").innerHTML = `
-        <form>
-        <h2>${current_question.question}</h2>
-          <input type="radio" value = "true" name = "true">
-          <label for="true">True</label><br>
-          <input type="radio" value = "false" name = "false">
-          <label for="false">False</label><br>
-          <input type ="submit" value = "Submit" id="submit">
-        </form>`
-  }
-  else if (current_question.question_type == "Fill in") {
-    document.getElementById("quiz_one_questions").innerHTML = `
       <form>
       <h2>${current_question.question}</h2>
-      <input type = "text" name = "answer">
-      <input type ="submit" value="Submit" id="submit"
+        <input type="radio" value = ${current_question.options.optiona} name = ${current_question.options.optiona}>
+        <label for=${current_question.options.optiona}> ${current_question.options.optiona}</label><br>
+        <input type="radio" value = ${current_question.options.optionb} name = ${current_question.options.optionb}>
+        <label for=${current_question.options.optionb}>${current_question.options.optionb}</label><br>
+        <input type="radio" value = ${current_question.options.optionc} name = ${current_question.options.optionc}>
+        <label for=${current_question.options.optionc}> ${current_question.options.optionc}</label><br>
+        <input type="radio" value = ${current_question.options.optiond} name=${current_question.options.optiond}>
+        <label>${current_question.options.optiond}</label><br>
+        <input type ="submit" value = "Submit" id="submit">
       </form>`
+    }
+    else if (question_type == "TF") {
+      document.getElementById("quiz_one_questions").innerHTML = `
+      <form>
+      <h2>${current_question.question}</h2>
+        <input type="radio" value = "true" name = "true">
+        <label for="true">True</label><br>
+        <input type="radio" value = "false" name = "false">
+        <label for="false">False</label><br>
+        <input type ="submit" value = "Submit" id="submit">
+      </form>`
+  }
+  else if (question_type == "Fill in") {
+    document.getElementById("quiz_one_questions").innerHTML = `
+    <form>
+    <h2>${current_question.question}</h2>
+    <input type = "text" value = "answer">
+    <input type = "submit" value = "submit" id= "submit"
+    </form>`
   }
   count = count + 1;
 }
-if(current_question == number_of_questions){
-  document.getElementById("quiz_one_questions").innerHTML = "Completes"
 }
-}
-
-
-
-function generate_quiz_two(data,current_question,number_of_questions){
-  for (var i = 0; i < number_of_questions; i++) {
-
-    if(current_question.question_type == "Multiple choice"){
-      document.getElementById("quiz_one_questions").innerHTML = `
-        <form>
-        <h2>${current_question.question}</h2>
-          <input type="radio" value = ${current_question.options.optiona} name = ${current_question.options.optiona}>
-          <label for=${current_question.options.optiona}> ${current_question.options.optiona}</label><br>
-          <input type="radio" value = ${current_question.options.optionb} name = ${current_question.options.optionb}>
-          <label for=${current_question.options.optionb}>${current_question.options.optionb}</label><br>
-          <input type="radio" value = ${current_question.options.optionc} name = ${current_question.options.optionc}>
-          <label for=${current_question.options.optionc}> ${current_question.options.optionc}</label><br>
-          <input type="radio" value = ${current_question.options.optiond} name=${current_question.options.optiond}>
-          <label>${current_question.options.optiond}</label><br>
-          <input type ="submit" value = "Submit" id="submit">
-        </form>`
-    }
-    else if (current_question.question_type == "TF") {
-      document.getElementById("quiz_one_questions").innerHTML = `
-        <form>
-        <h2>${current_question.question}</h2>
-          <input type="radio" value = "true" name = "true">
-          <label for="true">True</label><br>
-          <input type="radio" value = "false" name = "false">
-          <label for="false">False</label><br>
-          <input type ="submit" value = "Submit" id="submit">
-        </form>`
-  }
-  else if (current_question.question_type == "Fill in") {
-    document.getElementById("quiz_one_questions").innerHTML = `
-      <form>
-      <h2>${current_question.question}</h2>
-      <input type = "text" name = "answer">
-      <input type ="submit" value="Submit" id="submit"
-      </form>`
-  }
-  count = count + 1;
-}
-if(current_question == number_of_questions){
-  document.getElementById("quiz_one_questions").innerHTML = "Completes"
-}
-}
-
 function timed_encouragment(){
     setTimeout(encouragment)
     setTimeout(get_api_data('https://my-json-server.typicode.com/sguthrie16/CUS1172_Project_4/db'), 10000)
